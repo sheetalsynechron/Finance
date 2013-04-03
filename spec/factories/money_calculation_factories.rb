6861d26c308675ea13eeb2a38c7795aea684690f
Kiran Borse <Kiran.Borse@synechron.com>
2013-04-03 10:41:33 +0530

    Remove spec directory

--- spec/factories/money_calculation_factories.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-	factory :money_calculation do |m|
-		m.calculation_amount 1000
-		m.user_id 1
-		m.organisation_id 1
-	end
-end
--- spec/support/devise.rb
+++
@@ -1,5 +0,0 @@
-RSpec.configure do |config|
-  config.include Devise::TestHelpers, :type => :controller
-  # config.extend SignHelper, :type => :controller
-  #config.include Devise:SignHelper, :type => :controller
-end
